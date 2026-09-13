8a93ff61cf0f2f7abe2de2f93752055bdcac6a41
Fouche du Preez <fouche@limehouse.co.za>
2026-09-13 17:33:37 +0200

    initial commit

---
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -0,0 +1,30 @@
+package com.example.taskmanager.model;
+
+import com.example.taskmanager.enums.Category;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tasks")
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private boolean isCompleted = false;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    @JsonBackReference("user-tasks")
+    private User user;
+}
