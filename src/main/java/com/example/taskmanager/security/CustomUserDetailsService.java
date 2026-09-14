75e02147311481123b53cc294a28713e3cab0761
Fouche du Preez <fouche@limehouse.co.za>
2026-09-14 21:49:56 +0200

    added spring security

---
+++ src/main/java/com/example/taskmanager/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.taskmanager.security;
+
+import com.example.taskmanager.model.User;
+import com.example.taskmanager.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
